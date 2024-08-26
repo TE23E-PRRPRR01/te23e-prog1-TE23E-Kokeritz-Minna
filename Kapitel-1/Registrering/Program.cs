b421d34f4e37097edbb2c5e9062af23deddda4b2
Minna Kökeritz <minna.kokeritz@elev.ga.ntig.se>
2024-08-26 14:31:59 +0200

    Tränat på cw, cr + svenska tecken

---
+++ Kapitel-1/Registrering/Program.cs
@@ -0,0 +1,36 @@
+﻿//Program för att registrera förnamn och efternamn
+
+Console.Clear();
+Console.InputEncoding = System.Text.Encoding.Unicode;
+Console.OutputEncoding = System.Text.Encoding.Unicode;
+
+Console.WriteLine("Sign in");
+
+Console.Write("Ange Förnamn: ");
+string förnamn = Console.ReadLine();
+Console.Write("Ange Efternamn: ");
+string efternamn = Console.ReadLine();
+
+Console.WriteLine("Hej," + förnamn + " " + efternamn);
+Console.ReadLine();
+
+
+Console.Clear();
+Console.InputEncoding = System.Text.Encoding.Unicode;
+Console.OutputEncoding = System.Text.Encoding.Unicode;
+
+Console.WriteLine("Log in");
+
+Console.Write("Förnamn: ");
+string logFörnamn = Console.ReadLine();
+Console.Write("Efternamn: ");
+string logEfternamn = Console.ReadLine();
+
+if (logFörnamn==förnamn && logEfternamn ==efternamn)
+{
+    Console.WriteLine($"Välkommen {förnamn} {efternamn}");
+}
+else
+{
+    Console.WriteLine("Något gick fel, försök igen");
+}
