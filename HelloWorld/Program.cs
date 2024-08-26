2d51b11642e9cc4d85a0ddb791971d50ba04f571
Minna Kökeritz <minna.kokeritz@elev.ga.ntig.se>
2024-08-26 13:52:38 +0200

    Hello world- konsol program

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Clear();
+
+Console.BackgroundColor = ConsoleColor.DarkMagenta;
+Console.ForegroundColor = ConsoleColor.White;
+
+Console.WriteLine("Hej, Världen!");
+Console.WriteLine("Första C# prgrammet");
+Console.WriteLine("yay!");
+
+Console.WriteLine();
+Console.Write("Ditt namnn min gode...? ");
+string namn = Console.ReadLine();
+Console.WriteLine("Hej,"+ namn);
+Console.WriteLine();
+Console.WriteLine("Dags för frågesport...yay");
+Console.Write("Vad gillar du att göra på fritiden, " + namn + "?");
+string intresse = Console.ReadLine();
+Console.WriteLine("intressant, jag gillar också att" + intresse);
+
+Console.Write("Vilken gren på OS 2024 var best? ");
+string favGrenOS = Console.ReadLine();
+
+
