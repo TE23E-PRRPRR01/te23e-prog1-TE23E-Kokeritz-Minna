6316626c0d016c406fac4717cd8691d67a8cfe65
Minna Kökeritz <minna.kokeritz@elev.ga.ntig.se>
2024-08-26 14:46:00 +0200

    program kollar ålder :text färg + if satser

--- Kapitel-1/AgeControl/Program.cs
+++ Kapitel-1/AgeControl/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//program to check if a user is above required age 
+Console.Clear();
+Console.ForegroundColor=ConsoleColor.White;
+
+Console.Write("Enter your age: ");
+string ageString = Console.ReadLine();
+int age = int.Parse(ageString);
+if (age>= 16)
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine("Välkommen!");
+    Console.ForegroundColor=ConsoleColor.White;
+}
+else
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("You're not old enough to participate");
+    Console.ForegroundColor=ConsoleColor.White;
+}
