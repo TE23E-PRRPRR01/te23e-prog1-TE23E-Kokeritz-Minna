a4049c7dae7ce4d8bbb76926432afa57b1802618
Minna Kökeritz <minna.kokeritz@elev.ga.ntig.se>
2024-08-28 14:50:08 +0200

    Biluthyrning Var ma med var

---
+++ Kapitel-1/Bil_Uthyrning/Program.cs
@@ -0,0 +1,22 @@
+﻿//Program för att beräkna kostnaden för att hyra bil hos en biluthyrningsfirma.
+//Startavgiften är 500 kr
+//Kostnaden per mil är 5 kr
+//Kostnad per dag är 100 kr
+
+Console.Clear();
+Console.InputEncoding = System.Text.Encoding.Unicode;
+Console.OutputEncoding = System.Text.Encoding.Unicode;
+
+Console.Write("Hur många mil har du kört? ");
+String milString = Console.ReadLine();
+Console.Write("Hur många dagar har du kört? ");
+String dagString = Console.ReadLine();
+
+int mil = int.Parse(milString);
+int dag = int.Parse(dagString);
+int milKost = mil * 5;
+int dagKost = dag * 100;
+
+int kostnad = 500 + milKost + dagKost;
+
+Console.WriteLine($"Din totala kostnad är {kostnad} kr");
