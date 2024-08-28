9b19d452af12f5b1fff200b7545f0cfa0e02fe86
Minna Kökeritz <minna.kokeritz@elev.ga.ntig.se>
2024-08-28 20:56:11 +0200

    clean up Lucktext och sektid

--- Kapitel-1/Lucktext/Program.cs
+++ Kapitel-1/Lucktext/Program.cs
@@ -1,78 +1,30 @@
-﻿Console.Clear();
+﻿string Fråga(string frågeText) {
+    while (true) {
+        Console.Write(frågeText);
+        string svar = Console.ReadLine();
+        if (svar != "") return svar;
+    }
+}
+
+Console.Clear();
 Console.InputEncoding = System.Text.Encoding.Unicode;
 Console.OutputEncoding = System.Text.Encoding.Unicode;
 
-Console.Write("Ange en varelse: ");
-String varelse = Console.ReadLine();
-Console.Write("Ange ett adjectiv: ");
-String adjectiv = Console.ReadLine();
-Console.Write("Ange ett namn: ");
-String namn = Console.ReadLine();
-Console.Write("Ange ett pronomen eller title: ");
-String pronomen = Console.ReadLine();
-Console.Write("Ange ett egenskap: ");
-String egenskap = Console.ReadLine();
-Console.Write("Ange en aktivitet: ");
-String aktivitet = Console.ReadLine();
+String varelse,adjektiv,namn,pronomen,egenskap,aktivitet;
 
-if (varelse == adjectiv && adjectiv == namn && namn == pronomen && pronomen == egenskap && egenskap == aktivitet && aktivitet == varelse)
+while (true)
 {
-    Console.WriteLine("du sa samma sak för allt, försök igen");
+    varelse = Fråga("Ange en varelse:");
+    adjektiv = Fråga("Ange ett adjektiv:");
+    namn = Fråga("Ange ett namn:");
+    pronomen = Fråga("Ange ett pronomen:");
+    egenskap = Fråga("Ange en egenskap:");
+    aktivitet = Fråga("Ange en aktivitet:");
 
-    Console.Write("Ange en varelse: ");
-    varelse = Console.ReadLine();
-    Console.Write("Ange ett adjectiv: ");
-    adjectiv = Console.ReadLine();
-    Console.Write("Ange ett namn: ");
-    namn = Console.ReadLine();
-    Console.Write("Ange ett pronomen ekker title: ");
-    pronomen = Console.ReadLine();
-    Console.Write("Ange ett egenskap: ");
-    egenskap = Console.ReadLine();
-    Console.Write("Ange en aktivitet: ");
-    aktivitet = Console.ReadLine();
-}
-if (varelse == " ")
-{
-    Console.WriteLine("du skrev inget försök igen");
-    Console.Write("Ange en varelse: ");
-    varelse = Console.ReadLine();
-}
-
-if (adjectiv == " ")
-{
-    Console.WriteLine("du skrev inget försök igen");
-    Console.Write("Ange ett adjectiv: ");
-    adjectiv = Console.ReadLine();
-}
-
-if (namn == " ")
-{
-    Console.WriteLine("du skrev inget försök igen");
-    Console.Write("Ange ett namn: ");
-    namn = Console.ReadLine();
-}
-
-if (pronomen == " ")
-{
-    Console.WriteLine("du skrev inget försök igen");
-    Console.Write("Ange ett pronomen eller title: ");
-    pronomen = Console.ReadLine();
-}
-
-if (egenskap == " ")
-{
-    Console.WriteLine("du skrev inget försök igen");
-    Console.Write("Ange en egenskap: ");
-    egenskap = Console.ReadLine();
-}
-
-if (varelse == " ")
-{
-    Console.WriteLine("du skrev inget försök igen");
-    Console.Write("Ange en aktivitet: ");
-    aktivitet = Console.ReadLine();
-}
-Console.WriteLine($"Det var en gång en {adjectiv} {varelse}, som hette ,{namn}. {pronomen} var en {egenskap} {varelse} som gillade att {aktivitet}.");
-
-//Det var en gång en {adjektiv} {varelse}, som hette ,{namn}. {pronomen} var en {egenskap} {varelse} som gillade att {aktivitet}.
+    if (varelse == adjektiv && adjektiv == namn && namn == pronomen && pronomen == egenskap && egenskap == aktivitet && aktivitet == varelse) {
+       Console.WriteLine("du sa samma sak för allt, försök igen");
+       continue; 
+    }
+    break;
+} 
+Console.WriteLine($"Det var en gång en {adjektiv} {varelse}, som hette {namn}. {pronomen} var en {egenskap} {varelse} som gillade att {aktivitet}.");
--- Kapitel-1/Sektid/Program.cs
+++ Kapitel-1/Sektid/Program.cs
@@ -1,4 +1,8 @@
 ﻿//Konverterar ett x antal sekunder till timmar minuter och sekunder
+void WriteInColor(int  tid, ConsoleColor color) {
+    Console.ForegroundColor = color;
+    Console.Write(tid);
+}
 
 Console.Clear();
 Console.ForegroundColor = ConsoleColor.White;
@@ -13,19 +17,24 @@ int timme = minut/60;
 int minutRemain = minut%60;
 
 Console.Write($"{sek} sekunder är ");
-Console.ForegroundColor = ConsoleColor.Blue;
-Console.Write(timme);
+
+WriteInColor(timme,ConsoleColor.Blue );
 Console.ForegroundColor = ConsoleColor.White;
 Console.Write(" timmar ");
 
-Console.ForegroundColor = ConsoleColor.Red;
-Console.Write(minutRemain);
+WriteInColor(minutRemain,ConsoleColor.Red);
 Console.ForegroundColor = ConsoleColor.White;
 Console.Write(" minuter och ");
 
-Console.ForegroundColor = ConsoleColor.Green;
-Console.Write(sekRemain);
+WriteInColor(sekRemain,ConsoleColor.Green);
 Console.ForegroundColor = ConsoleColor.White;
 Console.Write(" sekunder");
 
 //Console.WriteLine($"{sek} sekunder är {timme} timmar {minutRemain} minuter och {sekRemain} sekunder");
+
+//Console.ForegroundColor = ConsoleColor.Blue;
+//Console.Write(timme);
+//Console.ForegroundColor = ConsoleColor.Red;
+//Console.Write(minutRemain);
+//Console.ForegroundColor = ConsoleColor.Green;
+//Console.Write(sekRemain);
