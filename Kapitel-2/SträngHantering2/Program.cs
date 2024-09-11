ea630e54d5489ddc1984591373c48abf66ca4083
Minna Kökeritz <minna.kokeritz@elev.ga.ntig.se>
2024-09-11 15:02:25 +0200

    Sträng hantering2 klar

--- Kapitel-2/SträngHantering2/Program.cs
+++ Kapitel-2/SträngHantering2/Program.cs
@@ -4,5 +4,6 @@ med #-tecken.*/
 
 Console.Clear();
 
-string mening = "Nog finns det mål och mening i vår färd - men det är vägen, som är mödan värd.";
-mening = mening.Replace(' ','#');
+string mening = " Nog finns det mål och mening i vår färd - men det är vägen, som är mödan värd. ";
+mening = mening.Trim().Replace(' ','#'); // tar bort blank spaces före och efter + erkätter alla mallanslag med #
+Console.WriteLine(mening); // skriver ut den nya meningen
