81f30112bcbbbe0f61673c535e818ad37eae29a9
Minna Kökeritz <minna.kokeritz@elev.ga.ntig.se>
2024-09-17 11:56:58 +0200

    stränghantering trim + leangth i foreach loopar

---
+++ Kapitel-2/SträngHantering6/Program.cs
@@ -0,0 +1,12 @@
+﻿/*Extrahera alla ord i meningen 
+"Ja visst gör det ont när knoppar brister." 
+som innehåller mer än 5 bokstäver.*/
+
+string mening = "Ja visst gör det ont när knoppar brister.";
+
+string [] ord = mening.Split();
+
+foreach (string i in ord)
+{
+    if (i.Trim('.').Length >= 5 ) Console.WriteLine(i.Trim('.').ToLower());
+}
