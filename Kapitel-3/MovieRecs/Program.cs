3d29bf23ec101284610adb42467b71fdb27920d0
Minna Kökeritz <minna.kokeritz@elev.ga.ntig.se>
2024-09-18 17:11:35 +0200

    Random number generator finnished

--- Kapitel-3/MovieRecs/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿// progamm som rekomenderar filmer baserat på vadanvändaren gillar
-
-Console.Clear();
-Console.WriteLine("Movie and series recs based on the users prefrences");
-
-Console.WriteLine("What genr");
---
+++ Kapitel-4/RandomTalUppgifter/TärningSimulator/Program.cs
@@ -0,0 +1,13 @@
+﻿/*Användaren ska kunna bestämma hur många tärningar de vill kasta, och hur många sidor varje tärning ska ha.
+Därefter ska programmet simulera tärningskasten och skriva ut resultatet av varje kast. */
+
+Console.Clear();
+
+Console.Write("Hur många tärningar vill du kasta: ");
+int antal = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < antal; i++)
+{
+    int tärning = Random.Shared.Next(1,7);
+    Console.WriteLine(tärning);
+}
